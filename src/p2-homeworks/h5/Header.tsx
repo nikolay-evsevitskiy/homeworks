6a37196087d669f6a0830c903cfb1762f624315f
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-02-05 23:41:42 +0300

    Done homeWork 5(Create sideBar)

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Button, Menu} from "antd";
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UserAddOutlined,
+    UserDeleteOutlined,
+    UserOutlined
+} from "@ant-design/icons";
+import { Link } from 'react-router-dom';
+import {PATH} from "./RoutesFunk";
+import 'antd/dist/antd.css'
 
-function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+
+function HeaderComponent() {
+    const [collapsed, setCollapsed] = useState<boolean>(false)
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed)
+    }
+    return <div>
+        <div style={{ width: 150 }}>
+            <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16}}>
+                {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+            </Button>
+            <Menu
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+                theme="dark"
+                inlineCollapsed={collapsed}
+            >
+                <Menu.Item key="1" icon={<UserDeleteOutlined />}>
+                    <Link to={PATH.PRE_JUNIOR}>Pre Junior</Link>
+                </Menu.Item>
+                <Menu.Item key="2" icon={<UserOutlined />}>
+                    <Link to={PATH.JUNIOR}> Junior</Link>
+                </Menu.Item>
+                <Menu.Item key="3" icon={<UserAddOutlined />}>
+                    <Link to={PATH.JUNIOR_PLUS}>Junior plus</Link>
+                </Menu.Item>
+            </Menu>
         </div>
-    )
+
+    </div>
+
+
 }
 
-export default Header
+export default HeaderComponent
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function JuniorPlus() {
+    return (
+        <div>
+            {/*<HW12/>*/}
+            {/*<HW13/>*/}
+            {/*<HW14/>*/}
+            {/*<HW15/>*/}
+            {/*<HW16/>*/}
+
+        </div>
+    )
+}
+
+export default JuniorPlus
