47103f77fe0d95a1f659e10839f2da22aeaebf5f
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-02-09 18:45:58 +0300

    homeWork 8(Create reducer and tests for it)

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,25 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {InitialStateType} from "../HW8";
+
+type ActionType = {
+    type: 'sort' | 'check'
+    payload: 'up' | 'down' | 18
+}
+
+export const homeWorkReducer = (state: Array<InitialStateType>, action: ActionType): Array<InitialStateType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            const newState = state.map(i => ({...i})).sort((a, b) =>
+                a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
+            if (action.payload === 'up') {
+                return newState
+            } else {
+                return newState.reverse()
+            }// need to fix
         }
         case 'check': {
-            // need to fix
-            return state
+            return action.payload === 18 ? state.filter(i => i.age >= 18) : state// need to fix
         }
-        default: return state
+        default:
+            return state
     }
 }
