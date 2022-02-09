6b122fdc64f6dd9d986800ce6844a06032eac228
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-02-10 00:53:33 +0300

    homeWork 10(create loading to 2 sec)

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+    const dispatch = useDispatch()
+    const loading = useSelector<AppStoreType, boolean>(state => state.loading.loading)// useSelector, useDispatch
+
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
+        dispatch(loadingAC(true))// dispatch
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 2000)// setTimeout
         console.log('loading...')
     };
 
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,24 @@
 const initState = {
-
+    loading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: InitialStateType = initState, action: ToggleLoadingAC): InitialStateType => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case 'TOGGLE_LOADING': {
+            return {...state, loading: action.value}
         }
-        default: return state
+        default:
+            return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (value: boolean) => ({
+    type: 'TOGGLE_LOADING',
+    value
+} as const)// fix any
+
+type InitialStateType = {
+    loading: boolean
+}
+
+type ToggleLoadingAC = ReturnType<typeof loadingAC>
